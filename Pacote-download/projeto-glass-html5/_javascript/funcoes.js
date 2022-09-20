13204fe5c46b2993544fe4de80e42c2b67071f80
Pietro de Moura Marins <112988401+PietroT10@users.noreply.github.com>
2022-09-19 22:37:37 -0300

    commit full course

---
+++ Pacote-download/projeto-glass-html5/_javascript/funcoes.js
@@ -0,0 +1,8 @@
+function mudaFoto(foto)
+{
+    document.getElementById("icone").src= foto;
+}
+var nome {
+var nome = window.prompt=('Qual é o seu nome?')
+window.alert=('É um prazer ter você no meu site, ' + nome + !)
+}
